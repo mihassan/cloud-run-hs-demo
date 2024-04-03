987fcd1d9285055f9c93bfe1d7a6823a6c91e46a
Md Imrul Hassan <mihassan@gmail.com>
2024-04-03 18:00:57 +1100

    Formatting.

--- lib/Api.hs
+++ lib/Api.hs
@@ -11,18 +11,21 @@ setupRoutes conn = do
   get "/quotes/random" $ do
     quote <- liftIO $ getRandomQuote conn
     json quote
+
   get "/quotes/:quote_id" $ do
     quote_id <- pathParam "quote_id"
     maybeQuote <- liftIO $ getQuote conn quote_id
     case maybeQuote of
       Just quote -> json quote
       Nothing -> throw . PathParameterNotFound $ "quote_id = " <> quote_id
+
   get "/quotes/category/:category/random" $ do
     category <- pathParam "category"
     maybeQuote <- liftIO $ getRandomQuoteByCategory conn category
     case maybeQuote of
       Just quote -> json quote
       Nothing -> throw . PathParameterNotFound $ "category = " <> category
+
   get "/categories" $ do
     categories <- liftIO $ getCategories conn
     json categories
