14df25706e4c7c5848e5f0167efd6c52375e4bcc
Md Imrul Hassan <mihassan@gmail.com>
2024-04-03 17:04:50 +1100

    Update API endpoint - /quotes/random to get a random quote, /quotes/:quote_id to get a specific quote.

--- lib/Api.hs
+++ lib/Api.hs
@@ -3,11 +3,17 @@
 module Api (setupRoutes) where
 
 import Database
-import Web.Scotty
 import Database.SQLite.Simple
+import Web.Scotty
 
 setupRoutes :: Connection -> ScottyM ()
 setupRoutes conn = do
-  get "/" $ do
+  get "/quotes/random" $ do
     quote <- liftIO $ getRandomQuote conn
     json quote
+  get "/quotes/:quote_id" $ do
+    quote_id <- pathParam "quote_id"
+    maybeQuote <- liftIO $ getQuote conn quote_id
+    case maybeQuote of
+      Just quote -> json quote
+      Nothing -> throw . PathParameterNotFound $ "quote_id = " <> quote_id
--- lib/Database.hs
+++ lib/Database.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Database (connectDb, getRandomQuote) where
+module Database (connectDb, getQuote, getRandomQuote) where
 
+import Data.Maybe
+import Data.Text
 import Database.SQLite.Simple
 import Quote
 
@@ -11,4 +13,9 @@ connectDb path = open path
 getRandomQuote :: Connection -> IO Quote
 getRandomQuote conn = do
   [quote] <- query_ conn "SELECT quote_id, author, quote, category FROM quotes ORDER BY RANDOM() LIMIT 1"
+  pure quote
+
+getQuote :: Connection -> Text -> IO (Maybe Quote)
+getQuote conn quote_id = do
+  quote <- listToMaybe <$> query conn "SELECT quote_id, author, quote, category FROM quotes WHERE quote_id = ?" (Only quote_id)
   pure quote
